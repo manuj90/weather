7abc73a383e389a957508596ed2cfd1ce81fa51f
ManuJ90 <juan.scagni@gmail.com>
2023-03-31 11:47:31 -0300

    js added

--- index.js
+++ index.js
@@ -1,77 +1,176 @@
-import { API_KEY } from './utils';
+import { API_KEY } from './utils.js';
 
-const container = document.querySelector('.container-box');
-const search = document.querySelector('.search-box button');
-const weatherBox = document.querySelector('.weather-box');
-const weatherDescription = document.querySelector('.weather-description');
-const error404 = document.querySelector('.not-found ');
+// Get elements
+const cityInput = document.getElementById('city-input');
+const searchButton = document.getElementById('search-button');
 
-search.addEventListener('click', () => {
-  const city = document.querySelector('.search-box input').value;
-  if (city === '') return;
-  searchCity(city);
-});
+// to be able to change the icon
+const weatherIcon = document.getElementById('weatherImg');
 
-const searchCity = (city) => {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
-  )
-    .then((response) => response.json())
-    .then((json) => {
-      if (json.cod === '404') {
-        container.style.height = '25rem';
-        weatherBox.style.display = 'none';
-        weatherDescription.style.display = 'none';
-        error404.style.display = 'block';
-        error404.classList.remove('hidden');
-        error404.classList.add('visible');
-        return;
-      }
+// get city
+const cityDisplay = document.getElementById('city');
+const weatherTypeDisplay = document.getElementById('weathertype');
+const feelsLikeDisplay = document.getElementById('feelsLike');
+const temperatureDisplay = document.getElementById('temperature');
 
-      error404.style.display = 'none';
-      error404.classList.remove('fadeIn');
+// humidity and windspeed and date
+const humidityDisplay = document.getElementById('humidity');
+const windSpeedDisplay = document.getElementById('windspeed');
+const dateDisplay = document.getElementById('date');
 
-      const image = document.querySelector('.weather-box img');
-      const location = document.querySelector('.weather-box .city');
-      const temperature = document.querySelector('.weather-box .temperature');
+// get chart and tabs
+const chartContainer = document.querySelector('#chart');
+const tabs = document.querySelectorAll('.tab');
 
-      const himidity = document.querySelector('.humidity');
-      const wind = document.querySelector('.wind');
+const temperatureData = [];
+const chartLabels = [];
+const labels = [];
 
-      switch (json.weather[0].main) {
-        case 'Clear':
-          image.src = '/images/clear.webp';
-          break;
+function addListeners() {
+  searchButton.addEventListener('click', () => {
+    const city = cityInput.value;
+    getWeatherData(city);
+  });
 
-        case 'Rain':
-          image.src = '/images/rain.webp';
-          break;
-
-        case 'Snow':
-          image.src = '/images/snow.webp';
-          break;
-
-        case 'Clouds':
-          image.src = '/images/cloud.webp';
-          break;
-
-        case 'Haze':
-          image.src = '/images/haze.webp';
-          break;
-
-        default:
-          image.src = '';
-      }
-
-      location.innerHTML = `${json.name}`;
-      temperature.innerHTML = `${parseInt(json.main.temp)} °C`;
-      himidity.innerHTML = `${json.main.humidity}%`;
-      wind.innerHTML = `${json.wind.speed} km/h`;
-
-      weatherBox.classList.remove('hidden');
-      weatherDescription.classList.remove('hidden');
-      weatherBox.classList.add('visible');
-      weatherDescription.classList.add('visible');
-      container.style.height = '30rem';
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      tabs.forEach((tab) => {
+        tab.classList.remove('active');
+      });
+      tab.classList.add('active');
+      updateChart(index);
     });
-};
+  });
+}
+
+async function fetchData(city) {
+  const [currentWeatherResponse, forecastResponse] = await Promise.all([
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    ),
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
+    ),
+  ]);
+
+  const currentWeatherData = await currentWeatherResponse.json();
+  const forecastData = await forecastResponse.json();
+
+  return [currentWeatherData, forecastData];
+}
+
+function drawChart() {
+  const chart = new Chart(chartContainer, {
+    type: 'line',
+    data: {
+      labels: chartLabels[0],
+      datasets: [
+        {
+          label: 'Temperature',
+          data: temperatureData[0],
+          backgroundColor: 'rgba(183, 216, 248, 1)',
+          borderColor: 'rgba(24, 90, 157, 1)',
+          borderWidth: 1,
+          pointRadius: 5,
+          pointHoverRadius: 10,
+          fill: true,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  window.chart = chart;
+}
+
+function updateText(city, currentWeatherData) {
+  cityDisplay.textContent = city;
+  temperatureDisplay.textContent = `${currentWeatherData.main.temp}°C`;
+  weatherTypeDisplay.textContent = `${currentWeatherData.weather[0].main}`;
+  feelsLikeDisplay.textContent = `Feels like ${currentWeatherData.main.feels_like}°C`;
+  humidityDisplay.textContent = `${currentWeatherData.main.humidity}%`;
+  windSpeedDisplay.textContent = `${currentWeatherData.wind.speed} km/h`;
+  const time = new Date().toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  });
+  const date = new Date().toLocaleDateString('en-us', {
+    weekday: 'short',
+    day: 'numeric',
+    month: 'short',
+  });
+  dateDisplay.textContent = date + ' ' + time;
+
+  weatherIcon.src = `http://openweathermap.org/img/wn/${currentWeatherData.weather[0].icon}@2x.png`;
+}
+
+function updateChart(index) {
+  chart.data.datasets[0].data = temperatureData[index];
+  chart.data.labels = chartLabels[index];
+  chart.update();
+}
+
+async function getWeatherData(city) {
+  // important to reset the arrays for every city
+  temperatureData.length = 0;
+  labels.length = 0;
+  chartLabels.length = 0;
+
+  const [currentWeatherData, forecastData] = await fetchData(city);
+
+  forecastData.list.forEach((item, i) => {
+    let date = new Date(item.dt * 1000);
+    let dateString = date.toLocaleDateString();
+    if (
+      i === 0 ||
+      dateString !==
+        new Date(forecastData.list[i - 1].dt * 1000).toLocaleDateString()
+    ) {
+      temperatureData.push([item.main.temp]);
+      labels.push(
+        date.toLocaleDateString('en-US', {
+          month: 'short',
+          day: 'numeric',
+        })
+      );
+      chartLabels.push([
+        date.toLocaleTimeString([], {
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false,
+        }),
+      ]);
+    } else {
+      temperatureData[temperatureData.length - 1].push(item.main.temp);
+      chartLabels[chartLabels.length - 1].push(
+        date.toLocaleTimeString([], {
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false,
+        })
+      );
+    }
+  });
+
+  updateText(city, currentWeatherData);
+  drawChart();
+
+  tabs[0].classList.add('active');
+  tabs.forEach((tab, index) => {
+    tab.textContent = labels[index];
+  });
+}
+
+addListeners();
+getWeatherData('Buenos Aires');
